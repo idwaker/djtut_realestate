99fd76e905e76ef8d431e832df408c384c98caa9
Diwaker Ghimire <idwaker@gmail.com>
2015-10-13 11:18:07 +0545

    added a view updated notebooks

--- property/models.py
+++ property/models.py
@@ -13,9 +13,9 @@ class House(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=9, decimal_places=2)
     address = models.TextField()
-    bedrooms = models.IntegerField()
-    kitchens = models.IntegerField()
-    bathrooms = models.IntegerField()
+    bedrooms = models.IntegerField(verbose_name='No of Bedrooms')
+    kitchens = models.IntegerField(verbose_name='No of Bedrooms')
+    bathrooms = models.IntegerField(verbose_name='No of Bedrooms')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- property/views.py
+++ property/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import House
 
-# Create your views here.
+
+def add(request):
+    """
+    This view shows how we can create a new instance and save to database
+    
+    Every view must have request argument, which is passed by django
+    """
+    house = House(
+        title='This is added and also duplicated',
+        description="""This is duplicated because
+        we don't have any field that is unique and everytime when someone
+        calls this method a new object with the same attributes will be created
+        and saved to database.""",
+        price=128.78,
+        address='This is address',
+        bedrooms=3,
+        bathrooms=2,
+        kitchens=1
+    )
+    house.save()
+    
+    # this is view so it must return a response
+    return HttpResponse('done')
